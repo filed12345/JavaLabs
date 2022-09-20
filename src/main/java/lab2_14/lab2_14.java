f106261203ae05292ce59fbdadb01d74e777b1d8
Veaceslav <filed12345@gmail.com>
2022-09-20 23:11:34 +0300

    first commit

---
+++ src/main/java/lab2_14/lab2_14.java
@@ -0,0 +1,15 @@
+package lab2_14;
+
+import java.util.Arrays;
+
+public class lab2_14 {
+    public static void main(String[] args) {
+        int x1 = 1;
+        int x2 = 2;
+        int x3 = 3;
+        int x4 = 4;
+        System.out.println(x1+" "+x2+" "+x3+" "+x4);
+        System.out.print(x1+" "+x2+" "+x3+" "+x4+"\n");
+        System.out.printf("%d %d %d %d",x1,x2,x3,x4);
+    }
+}
